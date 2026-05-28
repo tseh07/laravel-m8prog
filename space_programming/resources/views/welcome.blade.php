abede5f3f4659ed0fd8c7473160555d2920b1822
tseh07 <31096@ma-web.nl>
2026-05-28 14:51:24 +0200

    uitwerking

---
+++ space_programming/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Favorite Quote</title>
+</head>
+<body>
+    <p>
+        "The truth is you don't know what is going to happen tomorrow. nothing is guaranteed, so be grateful to live everyday."
+        <cite>— Edison Ho</cite>
+</p>
+    
+</body>
+</html>
